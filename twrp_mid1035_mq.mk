c2cd8ad5ba88090a2d915e1452d9c672fb1391ec
tracedgod <bowestrace@gmail.com>
2023-12-10 20:41:12 -0500

    disable GSI keys (again)

--- twrp_mid1035_mq.mk
+++ twrp_mid1035_mq.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
